8cc8dbb3a75a067139ffd2136dd8146c3121b646
ksh277 <ham5752@naver.com>
2025-10-16 21:51:19 +0900

    fix: Remove .js extension from database import in listings API

--- api/listings.ts
+++ api/listings.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { db } from '../utils/database.js';
+import { db } from '../utils/database';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   // CORS headers
