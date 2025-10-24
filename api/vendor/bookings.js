ba7aa33b2c48ab6a6ceab2b493ca493d7449dc44
ksh277 <ham5752@naver.com>
2025-10-24 09:55:00 +0900

    fix: Add CORS methods for vendor bookings API

--- api/vendor/bookings.js
+++ api/vendor/bookings.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 module.exports = async function handler(req, res) {
   // CORS 헤더
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
   if (req.method === 'OPTIONS') {
