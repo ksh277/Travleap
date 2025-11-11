0ac0f4c23bcd1b0fb45a6094f4476e310341d6b0
ksh277 <ham5752@naver.com>
2025-11-11 23:55:35 +0900

    fix: Add Authorization header to CORS allowed headers in accommodation booking API
    The API was rejecting requests with Authorization header because it wasn't in the Access-Control-Allow-Headers list. This was causing 'Method not allowed' errors even though the method was correct.

--- api/accommodations/book.js
+++ api/accommodations/book.js
@@ -8,7 +8,7 @@ const { connect } = require('@planetscale/database');
 module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
