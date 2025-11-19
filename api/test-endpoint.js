9c74567ffb4dc5d7f3f6e487cbc30dee6c66d86c
ksh277 <ham5752@naver.com>
2025-11-20 04:21:54 +0900

    test: Add simple test endpoint to verify serverless functions work

---
+++ api/test-endpoint.js
@@ -0,0 +1,10 @@
+module.exports = async function handler(req, res) {
+  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  return res.status(200).json({
+    success: true,
+    message: 'API is working!',
+    timestamp: new Date().toISOString()
+  });
+};
